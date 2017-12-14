d18461174a4f7dd1fd39e24625a974b770b73401
Ram Goli <ramsgoli@gmail.com>
2017-12-14 07:15:35 +0000

    Fix commands for production

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = {
   entry: {
     main: [
-      './src/main.js',
+      './src/index.js',
     ],
   },
   // Render source-map file for final build
