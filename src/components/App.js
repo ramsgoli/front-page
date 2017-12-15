f54a4ea5aa6c689aa3f90387aed62e2004dcc9cb
Ram Goli <ramsgoli@gmail.com>
2017-12-15 00:20:08 -0800

    add styles, some stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>- Ram Goli</p>
+        </div>
+    );
 }
 
 export default App;
