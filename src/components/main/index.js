62b84fb0b617b59e90bf8eceeb8f9e7cbf32753a
Ram Goli <ramsgoli@gmail.com>
2017-12-15 20:29:19 -0800

    setup main component

---
+++ src/components/main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import profilePic from '../../images/profile.JPG';
+
+const Main = () => {
+    return (
+        <div className="container-main">
+            <h1>Ram Goli</h1>
+            <div className="divider"></div>
+            <div className="profile-pic"><img src={profilePic} alt="profile-pic" /></div>
+        </div>
+    );
+}
+
+export default Main;
